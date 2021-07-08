7d5f3e6283769a813a2ae9e21e6fc16296779880
James Yngson <Sxulace@Jamess-MacBook-Pro.local>
2021-07-07 17:27:14 -0700

    Need to create a gallery

--- src/components/Vulkan/Vulkan.js
+++ src/components/Vulkan/Vulkan.js
@@ -1,10 +1,39 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Vulkan.scss";
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+import { Carousel } from "react-responsive-carousel";
+import { data } from "../../data/VulkanImageData";
 
 export default function Vulkan() {
+  const [galleryData] = useState(data);
+
   return (
     <div className="Vulkan">
-      <div className="Vulkan__topMargin"></div>
+      <Carousel
+        infiniteLoop={true}
+        autoPlay={true}
+        interval={6500}
+        showThumbs={false}
+      >
+        <div style={{ height: "100vh" }}>
+          <img src={galleryData[7].image} />
+        </div>
+        <div style={{ height: "100vh" }}>
+          <img src={galleryData[18].image} />
+        </div>
+        <div style={{ height: "100vh" }}>
+          <img src={galleryData[16].image} />
+        </div>
+      </Carousel>
+      <div className="Vulkan__lookbook">
+        <h1> Lookbook </h1>
+        <div className="Vulkan__gallery">
+          {/* {galleryData.map((card, id) => {
+            return (
+            );
+        } */}
+        </div>
+      </div>
     </div>
   );
 }
