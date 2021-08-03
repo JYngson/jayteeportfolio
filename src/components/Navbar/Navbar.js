e41d13b6eb32956c68fc705b234e316e3d75c3a9
James Yngson <Sxulace@Jamess-MacBook-Pro.local>
2021-08-02 17:38:01 -0700

    why it keep break

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -37,52 +37,62 @@ export default function Navbar({ setBackground }) {
   return (
     <div className="Navbar">
       <div className="Navbar__sidebar">
-        <FcMenu className="Navbar__sideIcon" onClick={mobileMenuHandle} />
-        <div className="Navbar__mainNav" style={{ display: mobileMenu }}>
-          <div className="Navbar__mobileNav">
-            <h2 className="Navbar__links" onClick={portfolioHandle}>
-              Portfolio
-            </h2>
-            <Link to="/about" className="Navbar__links">
-              About
-            </Link>
-          </div>
+        <div className="Navbar__iconBox" onClick={mobileMenuHandle}>
+          <FcMenu className="Navbar__sideIcon" />
         </div>
-
-        {portfolio ? (
-          <div className="Navbar__portfolio">
-            <div className="Navbar__portfolioShoots">
-              <div className="Navbar__shootSublist">
-                <h3 className="Navbar__shootSubheader" onClick={fashionHandle}>
-                  Fashion
-                </h3>
-                <h3 className="Navbar__shootSubheader" onClick={jewelryHandle}>
-                  Jewelry
-                </h3>
-              </div>
-              {sublist ? (
-                <div>
-                  <div className="Navbar__shootList">
-                    <ul>
-                      <Link
-                        to="/Vulkan"
-                        className="Navbar__shoot"
-                        onMouseEnter={() => onHover(`${Differentiate}`)}
-                        onMouseLeave={() => onHover("Black")}
-                      >
-                        01_21_VULKAN//17:23
-                      </Link>
-                    </ul>
-                  </div>
-                </div>
-              ) : (
-                <h2>Jewelry</h2>
-              )}
+        <div className="Navbar__mobileNav">
+          <div className="Navbar__mainNav" style={{ display: mobileMenu }}>
+            <div className="Navbar__mobileLinks">
+              <h2 className="Navbar__links" onClick={portfolioHandle}>
+                Portfolio
+              </h2>
+              <Link to="/about" className="Navbar__links">
+                About
+              </Link>
             </div>
           </div>
-        ) : (
-          <div />
-        )}
+
+          {portfolio ? (
+            <div className="Navbar__portfolio">
+              <div className="Navbar__portfolioShoots">
+                <div className="Navbar__shootSublist">
+                  <h3
+                    className="Navbar__shootSubheader"
+                    onClick={fashionHandle}
+                  >
+                    Fashion
+                  </h3>
+                  <h3
+                    className="Navbar__shootSubheader"
+                    onClick={jewelryHandle}
+                  >
+                    Jewelry
+                  </h3>
+                </div>
+                {sublist ? (
+                  <div>
+                    <div className="Navbar__shootList">
+                      <ul>
+                        <Link
+                          to="/Vulkan"
+                          className="Navbar__shoot"
+                          onMouseEnter={() => onHover(`${Differentiate}`)}
+                          onMouseLeave={() => onHover("Black")}
+                        >
+                          01_21_VULKAN//17:23
+                        </Link>
+                      </ul>
+                    </div>
+                  </div>
+                ) : (
+                  <h2>Jewelry</h2>
+                )}
+              </div>
+            </div>
+          ) : (
+            <div />
+          )}
+        </div>
       </div>
 
       <div className="Navbar__title">
