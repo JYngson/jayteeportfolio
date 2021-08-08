5d55dc35eb71472b0258e94f5703ddf7ccf7e033
James Yngson <Sxulace@Jamess-MacBook-Pro.local>
2021-08-07 18:21:45 -0700

    Need to update bio

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -29,20 +29,7 @@ export default function About() {
       variants={variants}
     >
       <div className="About__container">
-        <p className="About__bio">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent a
-          tristique felis, ac gravida nulla. Proin vel enim mollis, tincidunt
-          sapien sit amet, lobortis lorem. Aliquam quis auctor leo, vitae
-          scelerisque nisi. Nam convallis eu eros sed auctor. Vestibulum
-          venenatis eget ipsum vitae vulputate. Fusce eros mauris, ultricies sit
-          amet ipsum nec, interdum mattis dolor. Morbi quis sapien eget enim
-          eleifend placerat et interdum turpis. Sed gravida posuere eros. Aenean
-          rutrum aliquam lacus id condimentum. Cras sit amet orci at ante
-          pretium rhoncus ac quis eros. Integer euismod nulla ut enim volutpat,
-          at ultrices dui rutrum. Maecenas congue, ligula ac bibendum lacinia,
-          nisi ante finibus magna, non egestas metus ligula quis leo. Praesent
-          scelerisque malesuada nisi, in consectetur metus egestas quis.
-        </p>
+        <p className="About__bio"></p>
       </div>
     </motion.div>
   );
