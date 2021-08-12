02fb7092467af4e70e9ffd64d08541ba3e58d89d
James Yngson <Sxulace@Jamess-MacBook-Pro.local>
2021-08-11 17:07:27 -0700

    Work on grid.

--- src/components/Vulkan/Vulkan.js
+++ src/components/Vulkan/Vulkan.js
@@ -9,6 +9,9 @@ import { data } from "../../data/VulkanImageData.js";
 import background1 from "../../assets/images/Vulkan/IMG_9486.jpg";
 import background2 from "../../assets/images/SevenTeenTwentyThree/IMG_9850.JPG";
 import background3 from "../../assets/images/SevenTeenTwentyThree/IMG_2899.JPG";
+import mobileBack1 from "../../assets/images/Vulkan/IMG_0061.JPG";
+import mobileBack2 from "../../assets/images/Vulkan/IMG_9186.jpg_new_.jpg";
+import mobileBack3 from "../../assets/images/SevenTeenTwentyThree/s.3.jpg";
 
 const variants = {
   initial: {
@@ -43,6 +46,7 @@ export default function Vulkan() {
         autoPlay={true}
         interval={6500}
         showThumbs={false}
+        className="Vulkan__hero"
       >
         <div style={{ height: "100vh" }}>
           <img src={background1} alt="Background 1" />
@@ -54,6 +58,26 @@ export default function Vulkan() {
           <img src={background3} alt="Background 3" />
         </div>
       </Carousel>
+
+      <div className="Vulkan__mobileHero">
+        <img
+          className="Vulkan__mobileHero--img1"
+          src={mobileBack1}
+          alt="Background1"
+        />
+        <img
+          className="Vulkan__mobileHero--img2"
+          src={mobileBack2}
+          alt="Background2"
+        />
+        <img
+          className="Vulkan__mobileHero--img3"
+          src={mobileBack3}
+          alt="Background3"
+        />
+        {/* <h1 className="Vulkan__mobileHero--header">Vulkan.</h1> */}
+      </div>
+
       <div className="Vulkan__lookbook">
         <h1> Lookbook </h1>
         <SRLWrapper>
